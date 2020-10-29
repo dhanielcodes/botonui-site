b2837a9ed6bd67e2bfe766a2d01f14a9cfb498ed
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-10-29 15:54:33 +0100

    Ui add ons +

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,18 +1,32 @@
 <template>
     <div class="about">
-        <main>
-            <img class="mm dev" src="../assets/dev.svg" alt="" srcset="">
-            <h2 class="mm">Adekoya Daniel</h2>
-            <p class="mm">Creator of Boton</p>
-            <div class="link mm">
-                <a href="https://twitter.com/daniel_adekoya_" target="_blank"><img src="../assets/twitter.svg" alt="" srcset=""></a>
-                <a href="https://www.linkedin.com/in/daniel-adekoya-010810192/" target="_blank"><img src="../assets/linkedin.svg" alt="" srcset=""></a>
-                <a href="https://www.instagram.com/danieladekoya_/" target="_blank"><img src="../assets/instagram.svg" alt="" srcset=""></a>
-                <a href="https://devdanielcodes.vercel.app/" target="_blank"><img src="../assets/link.svg" alt="" srcset=""></a>
-            </div>
-        </main>
+        <div class="team">
+            <main>
+                <img class="mm dev" src="../assets/dev.svg" alt="" srcset="">
+                <h2 class="mm">Adekoya Daniel</h2>
+                <p class="mm">Creator of Boton</p>
+                <div class="link mm">
+                    <a href="https://twitter.com/daniel_adekoya_" target="_blank"><img src="../assets/twitter.svg" alt="" srcset=""></a>
+                    <a href="https://www.linkedin.com/in/daniel-adekoya-010810192/" target="_blank"><img src="../assets/linkedin.svg" alt="" srcset=""></a>
+                    <a href="https://www.instagram.com/danieladekoya_/" target="_blank"><img src="../assets/instagram.svg" alt="" srcset=""></a>
+                    <a href="https://devdanielcodes.vercel.app/" target="_blank"><img src="../assets/link.svg" alt="" srcset=""></a>
+                </div>
+            </main>
+            <main>
+                <img class="mm dev" src="../assets/peter.svg" alt="" srcset="">
+                <h2 class="mm">Amugen Peter</h2>
+                <p class="mm">Co-Developer</p>
+                <div class="link mm">
+                    <a href="https://twitter.com/daniel_adekoya_" target="_blank"><img src="../assets/twitter.svg" alt="" srcset=""></a>
+                    <a href="https://www.linkedin.com/in/daniel-adekoya-010810192/" target="_blank"><img src="../assets/linkedin.svg" alt="" srcset=""></a>
+                    <a href="https://www.instagram.com/danieladekoya_/" target="_blank"><img src="../assets/instagram.svg" alt="" srcset=""></a>
+                    <a href="https://devdanielcodes.vercel.app/" target="_blank"><img src="../assets/link.svg" alt="" srcset=""></a>
+                </div>
+            </main>
+        </div>
+        
         <div class="side">
-            <h1>Developer.</h1>
+            <h1>Developers.</h1>
         </div>
     </div>
 </template>
@@ -27,10 +41,12 @@ export default {
 main{
     width: 300px;
     margin: auto;
-    margin-top: 70px;
     text-align: center
-
- 
+}
+.team{
+    display: flex;
+    margin-top: 70px;
+    flex-wrap: wrap;
 }
 .dev{
     width: 200px;
@@ -42,17 +58,22 @@ main{
     margin: 0 5px;
 }
 .side{
-        color: white;
-        padding: 20px;
-        background-color: #789EB2;
-        font-size: clamp(20px, 5vw, 50px);
-        width: 100%;
-        text-align: center;
-        border-radius: 200px 200px 0 0;
-        margin: auto;
-        text-decoration: none;
-        font-weight: 600;
-        position: absolute;
-        bottom: 0
+    color: white;
+    padding: 20px;
+    background-color: #789EB2;
+    font-size: clamp(20px, 5vw, 50px);
+    width: 100%;
+    text-align: center;
+    border-radius: 200px 200px 0 0;
+    margin: auto;
+    text-decoration: none;
+    font-weight: 600;
+    position: fixed;
+    bottom: 0
+}
+@media screen and (max-width: 615px){
+    .side{
+        position: relative;
+    }
 }
 </style>
